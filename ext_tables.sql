ef786eb912ad3fe95e545062d16b787e6ff77324
Christian Dilger <c.dilger@addorange.de>
2026-03-20 15:27:47 +0100

    [FIX] salutation

--- ext_tables.sql
+++ ext_tables.sql
@@ -26,58 +26,58 @@ CREATE TABLE tx_rkwcompetition_domain_model_competition (
 );
 
 CREATE TABLE tx_rkwcompetition_domain_model_sector (
-   title varchar(255) NOT NULL DEFAULT '',
-   description text
+		title varchar(255) NOT NULL DEFAULT '',
+		description text
 );
 
 CREATE TABLE tx_rkwcompetition_domain_model_register (
-	salutation varchar(255) NOT NULL DEFAULT '',
-	title varchar(255) NOT NULL DEFAULT '',
-	first_name varchar(255) NOT NULL DEFAULT '',
-	last_name varchar(255) NOT NULL DEFAULT '',
-	institution varchar(255) NOT NULL DEFAULT '',
-	address varchar(255) NOT NULL DEFAULT '',
-	zip int(11) NOT NULL DEFAULT '0',
-	city varchar(255) NOT NULL DEFAULT '',
-    telephone varchar(255) DEFAULT '' NOT NULL,
-    email varchar(255) DEFAULT '' NOT NULL,
-	contribution_title varchar(255) NOT NULL DEFAULT '',
-	type_of_work int(11) NOT NULL DEFAULT '0',
-	remark text NOT NULL DEFAULT '',
-	privacy int(11) NOT NULL DEFAULT '0',
-	conditions_of_participation int(11) NOT NULL DEFAULT '0',
-	is_group_work int(11) NOT NULL DEFAULT '0',
-	group_work_authorization int(11) NOT NULL DEFAULT '0',
-	group_work_add_persons text NOT NULL DEFAULT '',
-    unique_id varchar(255) NOT NULL DEFAULT '',
-    own_cloud_folder_link varchar(255) NOT NULL DEFAULT '',
+		salutation int(11) DEFAULT '99' NOT NULL,
+		title varchar(255) NOT NULL DEFAULT '',
+		first_name varchar(255) NOT NULL DEFAULT '',
+		last_name varchar(255) NOT NULL DEFAULT '',
+		institution varchar(255) NOT NULL DEFAULT '',
+		address varchar(255) NOT NULL DEFAULT '',
+		zip int(11) NOT NULL DEFAULT '0',
+		city varchar(255) NOT NULL DEFAULT '',
+		telephone varchar(255) DEFAULT '' NOT NULL,
+		email varchar(255) DEFAULT '' NOT NULL,
+		contribution_title varchar(255) NOT NULL DEFAULT '',
+		type_of_work int(11) NOT NULL DEFAULT '0',
+		remark text NOT NULL DEFAULT '',
+		privacy int(11) NOT NULL DEFAULT '0',
+		conditions_of_participation int(11) NOT NULL DEFAULT '0',
+		is_group_work int(11) NOT NULL DEFAULT '0',
+		group_work_authorization int(11) NOT NULL DEFAULT '0',
+		group_work_add_persons text NOT NULL DEFAULT '',
+		unique_id varchar(255) NOT NULL DEFAULT '',
+		own_cloud_folder_link varchar(255) NOT NULL DEFAULT '',
 
-    user_submitted_at int(11) unsigned NOT NULL DEFAULT '0',
+		user_submitted_at int(11) unsigned NOT NULL DEFAULT '0',
 
-    admin_approved int(11) NOT NULL DEFAULT '0',
-    admin_approved_by int(11) NOT NULL DEFAULT '0',
-    admin_approved_at int(11) unsigned NOT NULL DEFAULT '0',
+		admin_approved int(11) NOT NULL DEFAULT '0',
+		admin_approved_by int(11) NOT NULL DEFAULT '0',
+		admin_approved_at int(11) unsigned NOT NULL DEFAULT '0',
 
-    admin_refused int(11) NOT NULL DEFAULT '0',
-    admin_refused_by int(11) NOT NULL DEFAULT '0',
-    admin_refused_text text NOT NULL DEFAULT '',
-    admin_refused_at int(11) unsigned NOT NULL DEFAULT '0',
+		admin_refused int(11) NOT NULL DEFAULT '0',
+		admin_refused_by int(11) NOT NULL DEFAULT '0',
+		admin_refused_text text NOT NULL DEFAULT '',
+		admin_refused_at int(11) unsigned NOT NULL DEFAULT '0',
 
-    admin_returned_by int(11) NOT NULL DEFAULT '0',
-    admin_returned_text text NOT NULL DEFAULT '',
-    admin_returned_at int(11) unsigned NOT NULL DEFAULT '0',
+		admin_returned_by int(11) NOT NULL DEFAULT '0',
+		admin_returned_text text NOT NULL DEFAULT '',
+		admin_returned_at int(11) unsigned NOT NULL DEFAULT '0',
 
-	upload int(11) unsigned DEFAULT '0',
+		upload int(11) unsigned DEFAULT '0',
 
-    sector int(11) NOT NULL DEFAULT '0',
-	competition int(11) unsigned DEFAULT '0' NOT NULL,
-    frontend_user int(11) unsigned DEFAULT '0' NOT NULL
+		sector int(11) NOT NULL DEFAULT '0',
+		competition int(11) unsigned DEFAULT '0' NOT NULL,
+		frontend_user int(11) unsigned DEFAULT '0' NOT NULL
 );
 
 CREATE TABLE tx_rkwcompetition_domain_model_upload (
-	abstract int(11) unsigned NOT NULL DEFAULT '0',
-	full int(11) unsigned NOT NULL DEFAULT '0',
-	remark text NOT NULL DEFAULT ''
+		abstract int(11) unsigned NOT NULL DEFAULT '0',
+		full int(11) unsigned NOT NULL DEFAULT '0',
+		remark text NOT NULL DEFAULT ''
 );
 
 CREATE TABLE tx_rkwcompetition_domain_model_juryreference (
@@ -87,7 +87,6 @@ CREATE TABLE tx_rkwcompetition_domain_model_juryreference (
     guest_user int(11) unsigned DEFAULT '0' NOT NULL,
     email varchar(255) DEFAULT '' NOT NULL,
     invite_token varchar(255) DEFAULT '' NOT NULL
-
 );
 
 
